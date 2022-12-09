fcc4959b094db77ea9e713e619189e53d188c50e
Oghie-Gabriel <oghieconsult@gmail.com>
2022-12-09 14:52:46 -0800

    check the code

---
+++ 0x00-hello_world/c-size.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - print the string in the put function
+ *
+ *   *
+ *
+ *    * Description: using the main function
+ *
+ *     * this program prints "Programming is like building a multilingual puzzle
+ *
+ *      * Return: 0
+ *
+ *       */
+
+int main(void)
+
+{
+
+		char c;
+
+			int i;
+
+				long li;
+
+					long long lli;
+
+						float f;
+
+
+
+							printf("Size of a char: %i byte(s)\n", sizeof(c));
+
+								printf("Size of an int: %i byte(s)\n", sizeof(i));
+
+									printf("Size of a long int: %i byte(s)\n", sizeof(li));
+
+										printf("Size of a long long int: %i byte(s)\n", sizeof(lli));
+
+											printf("Size of a float: %i byte(s)\n", sizeof(f));
+
+												return (0);
+
+}
