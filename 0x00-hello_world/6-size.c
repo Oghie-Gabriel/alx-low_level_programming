6b2f855516878c15f34616282910eaaca594294a
Oghie-Gabriel <oghieconsult@gmail.com>
2022-12-10 01:05:44 -0800

    changes on the code arrangement

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdlib.h>
 /** 
 * main: This is the entry point of the programme
 *Return: Always 0 (Success)
@@ -8,10 +9,15 @@ int main(void)
 {
 
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
+
 printf("Size of an int: %lu byte(s)\n", sizeof(int));
+
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+
 printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+
 printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
 return (0);
 
 }
