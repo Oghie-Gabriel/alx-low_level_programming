8039a35ca45483dc08b3edc8a6123ca6f7fefb65
Oghie-Gabriel <oghieconsult@gmail.com>
2022-12-10 00:47:44 -0800

    changes on the arrangement of the comment

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,7 @@
 #include <stdlib.h>
-
-// main - The entry point of the programme
- //Return: Always 0 (Success)
-
+/* main - The entry point of the programme
+Return: Always 0 (Success)
+*/
 int main(void)
 
 {
