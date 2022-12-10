7425e859f0ebb65700b236ba9275c80c41a0961d
Oghie-Gabriel <oghieconsult@gmail.com>
2022-12-10 01:31:12 -0800

    Little change on spaces at the comment section

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,6 @@
 *Return 0 to indicate that the program ran successfully
 */
 int main(void)
-
 {
 
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
@@ -13,7 +12,5 @@ printf("Size of an int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %lu byte(s)\n", sizeof(float));
-
 return (0);
-
 }
