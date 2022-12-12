5a9008222cd76277732c97135db6a03401e791a2
Oghie-Gabriel <oghieconsult@gmail.com>
2022-12-12 11:43:55 -0800

    Made major changes on the comment line

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,8 @@
 * Return: Always 1
 */
 int main(void)
+/** Prints the string to the standard error */
+
 {
 		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 		return (1);
