9e9100718ba0472f1f7bd3b255f0e17464df4108
Oghie-Gabriel <oghieconsult@gmail.com>
2022-12-11 19:44:45 -0800

    changes made on the comment line

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- *
- * * main - Entry point
- * * Return: Always 0 (Success)
+/*
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
