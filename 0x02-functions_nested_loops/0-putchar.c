aaa314fef05f7241b86c06f0c4bb25ab4d095080
Oghie-Gabriel <oghieconsult@gmail.com>
2022-12-14 13:51:46 -0800

    Script that prints _puchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
-* main - Entry point
-* Description: prints _putchar followed by new line
-* Return: 0
+* main - prints _putchar followed by a new line
+ * Return: Always 0 (Success)
 */
-
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int i;
 
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
-	return (0);
+	char word[] = "_putchar";
+	int counter;
+
+	for (counter = 0; counter < 8; counter++)
+{
+		_putchar(word[counter]);
+
+}
+_putchar('\n');
+return (0);
 }
