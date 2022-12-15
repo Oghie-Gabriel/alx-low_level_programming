8789cbcfaa818a44bcddc39c20af404c1da9d2da
Oghie-Gabriel <oghieconsult@gmail.com>
2022-12-14 20:13:08 -0800

    My tested loop file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,17 +4,17 @@
 * main - prints _putchar followed by a new line
  * Return: Always 0 (Success)
 */
+
 int main(void)
 {
+		char word[] = "_putchar";
+		int counter;
 
-	char word[] = "_putchar";
-	int counter;
-
-	for (counter = 0; counter < 8; counter++)
+		for (counter = 0; counter < 8; counter++)
 {
-		_putchar(word[counter]);
-
+			_putchar(word[counter]);
 }
-_putchar('\n');
-return (0);
+		_putchar('\n');
+		return (0);
+
 }
