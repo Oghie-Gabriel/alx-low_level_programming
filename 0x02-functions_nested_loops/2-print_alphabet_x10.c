60f2560b20799b9f8319f53c8c12add6f9e18157
Oghie-Gabriel <oghieconsult@gmail.com>
2022-12-14 21:07:23 -0800

    Changes made on the whitespace

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 
 /**
 * print_alphabet_x10 - a function that print 10 times the alphabet
- * Return: x10 a-z
+* Return: x10 a-z
 */
 void print_alphabet_x10(void)
 {
@@ -18,5 +18,5 @@ void print_alphabet_x10(void)
 		}
 		co++;
 		_putchar('\n');
-}
+		}
 }
