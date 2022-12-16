e89ba01d5e86ab0bf942e2f3beee73f132fe3b19
Oghie-Gabriel <80237978+Oghie-Gabriel@users.noreply.github.com>
2022-12-16 23:53:57 +0100

    Create 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle followed by new line
+ *@size: variable
+ * Return: Always 0
+ */
+void print_triangle(int size)
+{
+int count, i;
+count = 0;
+if (size > 0)
+{
+while (count < size)
+{
+i = size;
+while (i > count + 1)
+{
+i--;
+_putchar(' ');
+}
+while (i > 0)
+{
+_putchar('#');
+i--;
+}
+count++;
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
