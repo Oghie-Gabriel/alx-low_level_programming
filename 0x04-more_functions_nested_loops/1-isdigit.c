e1ed0ab2d6d68d96362fe090b25b84443c36306c
Oghie-Gabriel <oghieconsult@gmail.com>
2022-12-16 11:29:06 -0800

    function that checks digits

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,11 +5,11 @@
 * @c: The number to be checked
 * Return: 1 if the number is a digit, 0 otherwise
 */
+
 int _isdigit(int c)
 {
-
 		if (c >= '0' && c <= '9')
 			return (1);
 		else
-			return (0)
+			return (0);
 }
