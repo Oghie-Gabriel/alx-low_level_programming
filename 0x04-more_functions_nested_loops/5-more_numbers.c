bd52b56abc1e60cc9b4943207fd4ac786f8a5a73
Oghie-Gabriel <oghieconsult@gmail.com>
2022-12-16 13:08:50 -0800

    Changes on line spacing

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,18 +5,17 @@
 */
 
 void more_numbers(void)
-
 {
-		int num, count;
-		
-		for (count = 0; count <= 9; count++)
+	int num, count;
+	
+	for (count = 0; count <= 9; count++)
+	{
+		for (num = 0; num <= 14; num++)
 		{
-			for (num = 0; num <= 14; num++)
-			{
-				if (num > 9)
-					_putchar((num / 10) + '0');
-				_putchar((num % 10) + '0');
-			}
-			_putchar('\n');
+			if (num > 9)
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
 		}
+		_putchar('\n');
+	}
 }
