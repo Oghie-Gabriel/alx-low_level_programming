1139b7beec16b20a1dd2a1d7a3a3567b2cc10fe2
Oghie-Gabriel <oghieconsult@gmail.com>
2022-12-16 13:22:59 -0800

    Changes made on line spacing

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,12 +7,12 @@
 
 void print_line(int n)
 {
-		int len;
-		
-		if (n > 0)
-		{
-			for (len = 0; len < n; len++)
-				_putchar('_');
+	int len;
+	
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+			_putchar('_');
 		}
 		_putchar('\n');
 }
