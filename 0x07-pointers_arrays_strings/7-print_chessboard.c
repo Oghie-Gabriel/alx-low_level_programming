9e95976f0a0d37f60877d5a8bcf34e8a9cca19ce
Oghie-Gabriel <80237978+Oghie-Gabriel@users.noreply.github.com>
2023-01-04 03:51:34 +0100

    Update 7-print_chessboard.c
    Changes on trailing whitespace

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,25 +1,38 @@
 #include "main.h"
-#include <stdio.h>
+
+
+
 /**
- * print_chessboard - prints the chessboard
- * @a: the board to print
+ * print_chessboard - the main function
+ * @a: pointer to an array
+ *
+ * Returns: 0.
  */
 
 void print_chessboard(char (*a)[8])
-{
-	int i, j;
 
-	i = j = 0;
-	while (i < 8)
+{
+
+	int i;
+
+	int j;
+
+
+
+	for (i = 0; i < 8; i++)
+
 	{
-		j = 0;
-		while (j < 8)
+
+		for (j = 0; j < 8; j++)
+
 		{
+
 			_putchar(a[i][j]);
-			j++;
+
 		}
+
 		_putchar('\n');
-		i++;
+
 	}
 
 }
