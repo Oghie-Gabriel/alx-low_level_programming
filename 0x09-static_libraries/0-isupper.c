317c4352dd833276ca5d2999ee216a23da813fca
Oghie-Gabriel <80237978+Oghie-Gabriel@users.noreply.github.com>
2023-01-10 20:19:54 +0100

    Update 0-isupper.c
     Check if paramter c represents an uppercase letter

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,14 +1,16 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * _isupper - uppercase letters
- * @c: char to check
- *
- * Return: 0 or 1
+ * _isupper - Check if paramter c represents an uppercase letter.
+ * @c: Int representing a character
+ * Return: Always 0.
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
+	{
 		return (1);
-	else
+	}
 	return (0);
 }
