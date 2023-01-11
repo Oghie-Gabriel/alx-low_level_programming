5ffd279f6771d0d5769019497359a9875bd863fc
Oghie-Gabriel <80237978+Oghie-Gabriel@users.noreply.github.com>
2023-01-11 05:33:48 +0100

    Update 3-islower.c
    Check input character status

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,10 +1,17 @@
 #include "main.h"
+
 /**
- * _islower - checks for lowercase character
- * @c: the character to check
- * Return: 1 if c is lowercase, 0 otherwise
+ * _islower - check input char status (upper/lower).
+ *
+ * @c: - input char to be checked
+ *
+ * Return: 1 if case is lower, 0 otherwise.
  */
+
 int _islower(int c)
 {
-	return (c >= 'a' && c <= 'z');
+	if (c > 'Z')
+		return (1);
+	else
+		return (0);
 }
