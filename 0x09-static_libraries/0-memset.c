5be282665f53b613877740abd9871ab9cdee5d1b
Oghie-Gabriel <80237978+Oghie-Gabriel@users.noreply.github.com>
2023-01-11 05:09:36 +0100

    Update 0-memset.c
    Changes made on the trailing whitespace

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,20 +1,21 @@
-#include "main.h"
+#include "stdio.h"
+
 /**
- * _memset - fill a block of memory with a specific value
- * @s: starting address of memory to be filled
- * @b: the desired value
- * @n: number of bytes to be changed
+ *_memset - fills memory with a constant byte n amount of times
  *
- * Return: changed array with new value for n bytes
+ *@s: pointer that has the address of the memory to fill
+ *@b: constant byte that will be written into the momory
+ *@n: amount of times the byte will be written into the memory
+ *
+ *Return: address of the memory where thebyte was written
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	unsigned int i;
 
-	for (; n > 0; i++)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
-		n--;
 	}
 	return (s);
 }
