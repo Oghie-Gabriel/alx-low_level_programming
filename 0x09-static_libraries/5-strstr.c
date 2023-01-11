4951549149a046fcf10b1520c6afbb9fad5c852b
Oghie-Gabriel <80237978+Oghie-Gabriel@users.noreply.github.com>
2023-01-11 05:49:32 +0100

    Update 5-strstr.c
    Locate a substring within a string

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,26 +1,49 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * _strstr - Entry point
- * @haystack: input
- * @needle: input
- * Return: Always 0 (Success)
+ *_strstr - locates a substring within a string
+ *
+ *@haystack: pointer that contains the address of the string to be evaluated
+ *@needle: pointer that contains the string to evaluate @haystack
+ *
+ *Return: address of the located string in @haystack if no substring is found
+ *then retun NULL
  */
 char *_strstr(char *haystack, char *needle)
 {
-	for (; *haystack != '\0'; haystack++)
+	int i = 0;
+	int j = 0;
+	int a;
+	int compare = 0;
+
+	if (*needle == 0)
 	{
-		char *l = haystack;
-		char *p = needle;
-
-		while (*l == *p && *p != '\0')
-		{
-			l++;
-			p++;
-		}
-
-		if (*p == '\0')
-			return (haystack);
+		return (haystack);
 	}
-
+	while (haystack[i] != 0)
+	{
+		j = 0;
+		a = i;
+		while (needle[j] != 0)
+		{
+			if (needle[j] == haystack[a])
+			{
+				compare = 1;
+			}
+			else
+			{
+				compare = 0;
+				break;
+			}
+			j++;
+			a++;
+		}
+		if (compare == 1)
+		{
+			return (haystack + i);
+		}
+		i++;
+	}
 	return (0);
 }
