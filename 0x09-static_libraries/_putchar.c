65e611d1d96420976e19f9951bf754df20de67cb
Oghie-Gabriel <80237978+Oghie-Gabriel@users.noreply.github.com>
2023-01-11 05:56:39 +0100

    Update _putchar.c
    Writes a character to a stdout

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c : the character to print
- * Return : on success 1.
- * on error, -1
+ * _putchar - Writes the character c to stdout
+ * @c: the character to print 
+ *
+ * Return: on success 
  */
 int _putchar(char c)
 {
-    return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
