055ba18146f9a4d9165871a4a1be1603a4e85893
Oghie-Gabriel <80237978+Oghie-Gabriel@users.noreply.github.com>
2023-01-11 05:51:21 +0100

    Update 5-strstr.c
    Estimate the absolute value

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,49 +1,13 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- *_strstr - locates a substring within a string
- *
- *@haystack: pointer that contains the address of the string to be evaluated
- *@needle: pointer that contains the string to evaluate @haystack
- *
- *Return: address of the located string in @haystack if no substring is found
- *then retun NULL
+ * _abs - estimates the absolute value
+ * @n: input to be estimates
+ * Return: The absolute value of the input
  */
-char *_strstr(char *haystack, char *needle)
+int _abs(int n)
 {
-	int i = 0;
-	int j = 0;
-	int a;
-	int compare = 0;
-
-	if (*needle == 0)
-	{
-		return (haystack);
-	}
-	while (haystack[i] != 0)
-	{
-		j = 0;
-		a = i;
-		while (needle[j] != 0)
-		{
-			if (needle[j] == haystack[a])
-			{
-				compare = 1;
-			}
-			else
-			{
-				compare = 0;
-				break;
-			}
-			j++;
-			a++;
-		}
-		if (compare == 1)
-		{
-			return (haystack + i);
-		}
-		i++;
-	}
-	return (0);
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
 }
