eee60c8fd7ea7f6cddde854146b5e9f03858985a
Oghie-Gabriel <80237978+Oghie-Gabriel@users.noreply.github.com>
2023-01-12 23:39:26 +0100

    Create main.h
    Header file

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+void *malloc_checked(unsigned int);
+char *string_nconcat(char *, char *, unsigned int);
+void *_calloc(unsigned int, unsigned int);
+int *array_range(int, int);
+void *_realloc(void *, unsigned int, unsigned int);
+#endif
