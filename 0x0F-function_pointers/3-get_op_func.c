65b6912cd4f97fe395edcdbf0a0048f0f7faa250
Oghie-Gabriel <80237978+Oghie-Gabriel@users.noreply.github.com>
2023-01-18 22:05:39 +0100

    Create 3-get_op_func.c
    Write a program that performs simple operations.

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+* get_op_func - check description
+* Description: Selects the correct function to perform the op
+* @s:op
+* Return: a pointer to the function
+*/
+
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
+
+while (i < 5)
+{
+if (!strcmp(ops[i].op, s))
+{
+return (ops[i].f);
+}
+i++;
+}
+
+return (NULL);
+
+}
