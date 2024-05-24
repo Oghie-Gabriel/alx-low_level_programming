bb103771658a3065229fee40238a34199720b953
Oghie-Gabriel <80237978+Oghie-Gabriel@users.noreply.github.com>
2024-05-24 07:31:03 +0100

    Stacks, Queues - LIFO, FIFO

---
+++ monty/pop.c
@@ -0,0 +1,38 @@
+#include "monty.h"
+
+/**
+*f_pop - a program that prints the top
+*@head: stack head
+*@counter: line_number
+*Return: nothing
+*/
+void f_pop(stack_t **head, unsigned int counter)
+{
+
+		stack_t *h;
+
+
+
+		if (*head == NULL)
+
+		{
+
+			fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
+
+			fclose(bus.file);
+
+			free(bus.content);
+
+			free_stack(*head);
+
+			exit(EXIT_FAILURE);
+
+		}
+
+		h = *head;
+
+		*head = h->next;
+
+		free(h);
+
+}
---
+++ monty/stack.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+*f_stack - a program that prints the top
+*@head: stack head
+*@counter: line_number
+*Return: nothing
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+		(void)head;
+
+		(void)counter;
+
+		bus.lifi = 0;
+}
