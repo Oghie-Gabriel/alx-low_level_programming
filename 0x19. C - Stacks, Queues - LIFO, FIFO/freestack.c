6b21e2b4cab70a691e282a01a19f0b8ebc4a42fa
Oghie-Gabriel <80237978+Oghie-Gabriel@users.noreply.github.com>
2024-05-24 07:10:15 +0100

    Stacks, Queues - LIFO, FIFO

---
+++ 0x19. C - Stacks, Queues - LIFO, FIFO/freestack.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+*free_stack - a program that frees a doubly linked list
+*@head: head of the stack
+*/
+void free_stack(stack_t *head)
+{
+		stack_t *aux;
+
+		aux = head;
+		while (head)
+		{
+			aux = head->next;
+			free(head);
+			head = aux;
+		}
+}
